961574b0ac9b7030f789d121be1ee01ed8fe6747
Gezagn Bekele <programmergezy@gmail.com>
2026-05-31 01:57:04 +0300

    chore: configure fullstack deployment on Vercel

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["https://your-vercel-domain.vercel.app"] // ← CHANGE TO YOUR VERCEL URL
+        ? true // Allowing all for now
         : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
